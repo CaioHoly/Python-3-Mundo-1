643c98abaac0b8b135bf23381d5bddd66390211b
HolyOn <86932022+CaioHoly@users.noreply.github.com>
2021-08-16 21:34:06 -0300

    Update ex0035.py

--- ex0035.py
+++ ex0035.py
@@ -8,6 +8,6 @@ r2 = float(input('Segundo segmento: '))
 r3 = float(input('Terceiro segmento: '))
 
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
-    print('É um triãngulo')
+    print('É um triângulo')
 else:
     print('Não é um triângulo')
